cc8332d206759db0aa5252e8b90cd44857af7b56
shrimpyvongue <shrimpyvongue@gmail.com>
2024-08-28 22:09:48 +0530

    Remove Previous Implementation Instead Use Datastore to Read and Save Last Connected Device.

--- core/data/src/main/java/komu/seki/data/repository/PreferencesRepositoryImpl.kt
+++ core/data/src/main/java/komu/seki/data/repository/PreferencesRepositoryImpl.kt
@@ -8,7 +8,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
-import komu.seki.domain.models.DeviceDetails
+import komu.seki.domain.models.DeviceInfo
 import komu.seki.domain.repository.PreferencesRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -22,28 +22,12 @@ class PreferencesDatastore @Inject constructor(
 ) : PreferencesRepository {
 
     private object DataPreferencesKeys{
-        val DEVICE_NAME = stringPreferencesKey("serviceName")
-        val HOST_ADDRESS = stringPreferencesKey("hostAddress")
         val SYNC_STATUS = booleanPreferencesKey("syncStatus")
+        val LAST_CONNECTED = stringPreferencesKey("lastConnected")
     }
 
     private val datastore = context.dataStore
 
-
-    override suspend fun saveDeviceDetails(deviceName: String, hostAddress: String) {
-        datastore.edit { settings ->
-            settings[DataPreferencesKeys.DEVICE_NAME] = deviceName
-            settings[DataPreferencesKeys.HOST_ADDRESS] = hostAddress
-        }
-    }
-
-    override fun readDeviceDetails(): Flow<DeviceDetails> {
-        return datastore.data.map { preferences ->
-            val deviceName = preferences[DataPreferencesKeys.DEVICE_NAME]
-            val host = preferences[DataPreferencesKeys.HOST_ADDRESS]
-            DeviceDetails(deviceName, host)
-        }
-    }
 
     override suspend fun saveSynStatus(syncStatus: Boolean) {
         datastore.edit { status->
@@ -54,6 +38,18 @@ class PreferencesDatastore @Inject constructor(
     override fun readSyncStatus(): Flow<Boolean> {
         return datastore.data.map { status ->
             status[DataPreferencesKeys.SYNC_STATUS] ?: false
+        }
+    }
+
+    override suspend fun saveLastConnected(hostAddress: String) {
+        datastore.edit { status->
+            status[DataPreferencesKeys.LAST_CONNECTED] = hostAddress
+        }
+    }
+
+    override fun readLastConnected(): Flow<String?> {
+        return datastore.data.map { host ->
+            host[DataPreferencesKeys.LAST_CONNECTED]
         }
     }
 }
--- core/domain/src/main/java/komu/seki/domain/repository/PreferencesRepository.kt
+++ core/domain/src/main/java/komu/seki/domain/repository/PreferencesRepository.kt
@@ -1,13 +1,13 @@
 package komu.seki.domain.repository
 
-import komu.seki.domain.models.DeviceDetails
+import komu.seki.domain.models.DeviceInfo
 import kotlinx.coroutines.flow.Flow
 
 interface PreferencesRepository {
-    suspend fun saveDeviceDetails(deviceName: String, hostAddress: String)
-    fun readDeviceDetails(): Flow<DeviceDetails>?
-
     suspend fun saveSynStatus(syncStatus: Boolean)
     fun readSyncStatus(): Flow<Boolean>
+
+    suspend fun saveLastConnected(hostAddress: String)
+    fun readLastConnected(): Flow<String?>
 }
 
