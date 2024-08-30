fe410046779cda8ea21bf1ec12b8d64b44002efe
shrimpyvongue <shrimpyvongue@gmail.com>
2024-08-30 19:30:02 +0530

    Fallback to NsdDiscovery for connection failure.

--- app/src/main/java/com/komu/sekia/services/WebSocketService.kt
+++ app/src/main/java/com/komu/sekia/services/WebSocketService.kt
@@ -1,6 +1,5 @@
 package com.komu.sekia.services
 
-import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -12,13 +11,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.net.nsd.NsdServiceInfo
+import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import android.os.BatteryManager
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.provider.Settings.Global
-import android.provider.Settings.Secure
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,8 +31,10 @@ import androidx.core.app.NotificationCompat.EXTRA_NOTIFICATION_ID
 import com.komu.sekia.MainActivity
 import com.komu.sekia.R
 import dagger.hilt.android.AndroidEntryPoint
-import komu.seki.data.database.Device
+import komu.seki.common.util.getWifiSSid
+import komu.seki.data.database.Network
 import komu.seki.data.repository.AppRepository
+import komu.seki.data.services.NsdService
 import komu.seki.domain.models.DeviceInfo
 import komu.seki.domain.models.DeviceStatus
 import komu.seki.domain.models.SocketMessage
@@ -40,6 +45,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -57,6 +64,9 @@ class WebSocketService : Service() {
     @Inject
     lateinit var context: Context
 
+    @Inject
+    lateinit var nsdService: NsdService
+
     private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     private val binder = LocalBinder()
 
@@ -65,8 +75,16 @@ class WebSocketService : Service() {
 
     private var lastBatteryLevel: Int? = null
 
+    private lateinit var connectivityManager: ConnectivityManager
+
     inner class LocalBinder : Binder() {
         fun getService(): WebSocketService = this@WebSocketService
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        registerReceivers()
     }
 
     override fun onBind(intent: Intent): IBinder {
@@ -75,53 +93,53 @@ class WebSocketService : Service() {
 
     private val batteryReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            sendDeviceStatus()
+            if (isConnected) {
+                sendDeviceStatus()
+            }
         }
     }
 
     private val wifiReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            sendDeviceStatus()
+            if (isConnected) {
+                sendDeviceStatus()
+            }
         }
     }
 
     private val bluetoothReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            sendDeviceStatus()
+            if (isConnected) {
+                sendDeviceStatus()
+            }
         }
     }
 
-    override fun onCreate() {
-        super.onCreate()
-
-        // Register Receivers
-        registerReceiver(batteryReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+    private fun registerReceivers() {
         registerReceiver(wifiReceiver, IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION))
+        registerReceiver(batteryReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
         registerReceiver(bluetoothReceiver, IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED))
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        val hostAddress = intent.getStringExtra(EXTRA_HOST_ADDRESS)
-        val newDevice = intent.getBooleanExtra(NEW_DEVICE, false)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val hostAddress = intent?.getStringExtra(EXTRA_HOST_ADDRESS)
+        val newDevice = intent?.getBooleanExtra(NEW_DEVICE, false) ?: false
         Log.d("WebSocketService", "Received hostAddress: $hostAddress new Device: $newDevice")
-        Log.d("WebSocketService", "Received action: ${intent.action}")
-        when (intent.action) {
-            Actions.START.name -> start(hostAddress!!, newDevice)
+        Log.d("WebSocketService", "Received action: ${intent?.action}")
+        when (intent?.action) {
+            Actions.START.name -> hostAddress?.let { start(it, newDevice) }
             Actions.STOP.name -> stop()
             else -> Log.d("WebSocketService", "Unknown action received")
         }
         return START_NOT_STICKY
     }
 
-
-
     private fun start(hostAddress: String, newDevice: Boolean) {
-        onCreate()
         scope.launch {
             try {
-                val deviceStatus = getDeviceStatus(context)
+                val deviceStatus = getDeviceStatus()
                 if (newDevice) {
-                    val deviceInfo = getDeviceInfo(context)
+                    val deviceInfo = getDeviceInfo()
                     isConnected = connect(hostAddress, deviceInfo)
                 } else {
                     isConnected = connect(hostAddress)
@@ -136,6 +154,9 @@ class WebSocketService : Service() {
                     Log.d("service", deviceStatus.toString())
                     sendMessage(deviceStatus)
                     sendActiveNotifications()
+                } else {
+                    stopNotificationService()
+                    registerNetworkCallback()
                 }
             } catch (e: Exception) {
                 Log.e("WebSocketService", "Error in WebSocket connection", e)
@@ -146,7 +167,7 @@ class WebSocketService : Service() {
     private fun sendDeviceStatus() {
         scope.launch {
             try {
-                val deviceStatus = getDeviceStatus(context)
+                val deviceStatus = getDeviceStatus()
                 val currentBatteryLevel = deviceStatus.batteryStatus
 
                 // Only send the status if the battery level has changed
@@ -162,43 +183,130 @@ class WebSocketService : Service() {
 
     private fun stop() {
         scope.launch {
+            stopNotificationService()
             disconnect()
             stopForeground(true)
             stopSelf()
             preferencesRepository.saveSynStatus(false)
-            isForegroundStarted = false // Reset the flag
+            isForegroundStarted = false
         }
+    }
+
+    private fun stopNotificationService() {
+        val stopNotificationServiceIntent = Intent(this, NotificationService::class.java)
+        stopNotificationServiceIntent.action = ACTION_STOP_NOTIFICATION_SERVICE
+        sendBroadcast(stopNotificationServiceIntent)
     }
 
     private suspend fun connect(hostAddress: String, deviceInfo: DeviceInfo? = null): Boolean {
         return webSocketRepository.connect(hostAddress, deviceInfo)
     }
 
+    private val networkCallback = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        object : ConnectivityManager.NetworkCallback(FLAG_INCLUDE_LOCATION_INFO) {
+            override fun onCapabilitiesChanged(
+                network: android.net.Network,
+                networkCapabilities: NetworkCapabilities
+            ) {
+                scope.launch {
+                    val wifiInfo = networkCapabilities.transportInfo as? WifiInfo
+                    if (wifiInfo != null && wifiInfo.ssid != "<unknown ssid>") {
+                        Log.d("WebSocketRepository", "ssid: ${wifiInfo.ssid}")
+                        val knownNetwork = appRepository.getNetwork(wifiInfo.ssid)
+                        if (knownNetwork != null) {
+                            startNSDDiscovery()
+                        }
+                    }
+                }
+                connectivityManager.unregisterNetworkCallback(this)
+            }
 
-    @SuppressLint("HardwareIds")
-    private fun getDeviceInfo(context: Context): DeviceInfo {
+            override fun onUnavailable() {
+                Log.d("WebSocketRepository", "Network unavailable")
+                connectivityManager.unregisterNetworkCallback(this)
+            }
+        }
+    } else {
+        object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: android.net.Network) {
+                super.onAvailable(network)
+                scope.launch {
+                    val wifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
+                    val wifiInfo = wifiManager.connectionInfo
+                    val ssid = wifiInfo?.ssid?.removeSurrounding("\"")
+                    if (ssid != null) {
+                        val knownNetwork = appRepository.getNetwork(ssid)
+                        if (knownNetwork != null) {
+                            startNSDDiscovery()
+                        }
+                    }
+                }
+            }
+
+            override fun onUnavailable() {
+                Log.d("WebSocketRepository", "Network unavailable")
+                connectivityManager.unregisterNetworkCallback(this)
+            }
+        }
+    }
+
+    private fun registerNetworkCallback() {
+        val networkRequest = NetworkRequest.Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .build()
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    private fun startNSDDiscovery() {
+        scope.launch {
+            nsdService.startDiscovery()
+            val discoveredServices = mutableSetOf<String>() // Set to track unique services
+            nsdService.services.collect { services ->
+                if (services.isNotEmpty()) {
+                    for (service in services) {
+                        if (discoveredServices.add(service.serviceName)) { // Add to set if it's a new service
+                            Log.d("service", service.serviceName)
+                            val hostAddress = appRepository.getHostAddress(service.serviceName)
+                            if (hostAddress != null) {
+                                if (isForegroundStarted) {
+                                    start(hostAddress, false)
+                                } else {
+                                    startForeground(NOTIFICATION_ID, createNotification())
+                                    delay(500)
+                                    start(hostAddress, false)
+                                }
+                            }
+                        } else {
+                            Log.d("service", "Duplicate service skipped: ${service.serviceName}")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun getDeviceInfo(): DeviceInfo {
         val deviceName = Global.getString(context.contentResolver, "device_name")
-//        val androidId = Secure.getString(context.contentResolver, Secure.ANDROID_ID)
         return DeviceInfo(
-//            id = androidId,
             deviceName = deviceName,
             userAvatar = null,
         )
     }
 
-    private fun getDeviceStatus(context: Context): DeviceStatus {
+    private fun getDeviceStatus(): DeviceStatus {
         val batteryStatus: Int? =
-            context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
                 ?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
 
         val isCharging = batteryStatus != null &&
-                context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+                registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
                     ?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) == BatteryManager.BATTERY_STATUS_CHARGING
 
-        val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val wifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
         val wifi = wifiManager.isWifiEnabled
 
-        val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         val bluetooth = bluetoothManager.adapter.isEnabled
 
         return DeviceStatus(
@@ -266,14 +374,18 @@ class WebSocketService : Service() {
         sendBroadcast(intent)
     }
 
-
     override fun onDestroy() {
         Log.d("WebSocketService", "onDestroy called")
         scope.launch {
             preferencesRepository.saveSynStatus(false)
         }
+        // Register Receivers
+        unregisterReceiver(batteryReceiver)
+        unregisterReceiver(wifiReceiver)
+        unregisterReceiver(bluetoothReceiver)
         scope.cancel()
         isForegroundStarted = false
+        super.onDestroy()
     }
 
     companion object {
@@ -281,11 +393,9 @@ class WebSocketService : Service() {
         const val EXTRA_HOST_ADDRESS = "extra_host_address"
         const val NEW_DEVICE = "new_device"
         const val ACTION_SEND_ACTIVE_NOTIFICATIONS = "com.komu.sekia.services.NotificationService.SEND_ACTIVE_NOTIFICATIONS"
+        const val ACTION_STOP_NOTIFICATION_SERVICE = "com.komu.sekia.services.NotificationService.STOP_NOTIFICATION_SERVICE"
     }
-
-
 }
-
 
 enum class Actions {
     START,
